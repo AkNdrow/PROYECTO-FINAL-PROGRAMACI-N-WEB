0bfdcc79bcf6c0ce6668e27bce8c15c81ebbce2e
moisep204 <moisep204@gmail.com>
2026-07-30 07:20:41 -0600

    Usuario

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -24,6 +24,42 @@ class UserSeeder extends Seeder
                 'role_id' => 1,
             ]
         );
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // 1. Administrador (Rol 1)
+        User::create([
+            'name' => 'Admin CleverNote',
+            'email' => 'admin@clevernote.com',
+            'password' => Hash::make('Admin#2026!'),
+            'role_id' => 1,
+        ]);
+
+        // 2. Cliente / Estándar (Rol 2)
+        User::create([
+            'name' => 'Cliente Prueba',
+            'email' => 'cliente@clevernote.com',
+            'password' => Hash::make('Cliente#2026!'),
+            'role_id' => 2,
+        ]);
+
+        // 3. Developer / Editor (Rol 3)
+        User::create([
+            'name' => 'Developer Prueba',
+            'email' => 'dev@clevernote.com',
+            'password' => Hash::make('Developer#2026!'),
+            'role_id' => 3,
+        ]);
+    
 
         // Crear 14 usuarios comunes (Clientes)
         User::factory(14)->create(['role_id' => 2]);
